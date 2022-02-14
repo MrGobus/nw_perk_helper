a9fcdb1f24b8d1fba36c9bb9c118a29e50894f66
Your Name <you@example.com>
2022-02-14 22:18:20 +0300

    кольца и амулет longer

--- src/PerkInfo/index.js
+++ src/PerkInfo/index.js
@@ -208,6 +208,7 @@ const Jewelry = props => <Box sx = {{flexGrow: 1}}>
     <CardsBlock filter = {props.name} title = "Perks" perks = {nw} />
 
     <CardsBlock filter = {props.name + " damage"} title = "Perks: damage" perks = {nw} />
+    <CardsBlock filter = {props.name + " longer"} title = "Perks: longer" perks = {nw} />
 </Box>
 
 const Tool = props => <Box sx = {{flexGrow: 1}}>

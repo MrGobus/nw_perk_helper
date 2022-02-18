4a88d6bc842fbace914230f3037aeb0f7c9918e6
Your Name <you@example.com>
2022-02-18 11:56:50 +0300

    earring mastery

--- src/PerkInfo/index.js
+++ src/PerkInfo/index.js
@@ -211,6 +211,7 @@ const Jewelry = props => <Box sx = {{flexGrow: 1}}>
     <CardsBlock filter = {props.name + " longer"} title = "Perks: longer" perks = {nw} />
     <CardsBlock filter = {props.name + " protection"} title = "Perks: protection" perks = {nw} />
     <CardsBlock filter = {props.name + " gathering"} title = "Perks: gathering" perks = {nw} />
+    <CardsBlock filter = {props.name + " mastery"} title = "Perks: mastery" perks = {nw} />
 </Box>
 
 const Tool = props => <Box sx = {{flexGrow: 1}}>

c5472c390bce935b473d755ca91f075c9199623c
Your Name <you@example.com>
2022-02-19 10:57:23 +0300

    add rapier and hatchet info

--- src/PerkInfo/WeaponInfo/index.js
+++ src/PerkInfo/WeaponInfo/index.js
@@ -2,9 +2,9 @@ import {
     Typography
 } from '@mui/material'
 
-// Slash attacks: Angry Earth +20%, Ancient -15%<br />
-// Trust attacks: Corrupted +20%, Beasts +20%, Angry Earth -15%, Lost -15%
-// Strike attacks: Lost +10%, Ancient +20%, Corrupted -15% 
+const SlashDamageInfo = props => <inline>Slash attacks: Angry Earth +20%, Ancient -15%</inline>
+const ThrustDamageInfo = props => <inline>Thrust attacks: Corrupted +20%, Beasts +20%, Angry Earth -15%, Lost -15%</inline>
+const StrikeDamageInfo = props => <inline>Strike attacks: Lost +10%, Ancient +20%, Corrupted -15% </inline>
 
 const perk_info = [
     {
@@ -23,8 +23,47 @@ const perk_info = [
                 <li>Charged Heavy Attack: Deals 160% Trust damage.</li>
             </ul>
             <p>
-                Slash attacks: Angry Earth +20%, Ancient -15%<br />
-                Trust attacks: Corrupted +20%, Beasts +20%, Angry Earth -15%, Lost -15%
+                <SlashDamageInfo /><br />
+                <ThrustDamageInfo />
+            </p>
+        </div>
+    },
+    {
+        name: "rapier",
+        body: <div>
+            <Typography variant="h3"component="div" sx = {{ mb: 2, mt: 2}}>
+                Rapier
+            </Typography>
+            <p>
+                A speed oriented melee weapon with a very quick attacks. Excels at precise timing and outmaneuvering opponents while applying bleeds that gradually drain the target's health.
+            </p>
+            <ul>
+                <li>Light Attack: Deals 100% Thrust damage.</li>
+                <li>Light Middle Swipes: Deals 66% Thrust damage.</li>
+                <li>Heavy Attack: Deals 133% Thrust damage.</li>
+            </ul>
+            <p>
+                <ThrustDamageInfo />
+            </p>
+        </div>
+    },
+    {
+        name: "hatchet",
+        body: <div>
+            <Typography variant="h3"component="div" sx = {{ mb: 2, mt: 2}}>
+                Hatchet
+            </Typography>
+            <p>
+                A diverse weapon that allows for ranged throwing attacks or rapid close quarters attacks, ideal for aggressive, fully committed, close quartes melee or tactical medium range combat.
+            </p>
+            <ul>
+                <li>Light Attack: Deals 100% Slash damage.</li>
+                <li>Light Ender: Deals 105% Slash damage.</li>
+                <li>Heavy Attack: Deals 120% Slash damage.</li>
+                <li>Charged Heavy Attack: Deals 160% Slash damage.</li>
+            </ul>
+            <p>
+                <SlashDamageInfo />
             </p>
         </div>
     },
